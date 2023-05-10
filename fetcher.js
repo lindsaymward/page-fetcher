0ed1349ee9524ca6801c7925d754ce33a8542d39
Lindsay Ward <lward23@hotmail.com>
2023-05-10 13:19:10 -0700

    Install request for HTTP request

---
+++ fetcher.js
@@ -0,0 +1,9 @@
+// Take in two command line arguments
+// 1. a URL
+// 2. a local file path
+
+// -> download the resource at the URL to the local path on your machine
+// -> Upon completion, print out "Downloaded and saved 1235 bytes to ./index.html"
+
+const request = require('request');
+
