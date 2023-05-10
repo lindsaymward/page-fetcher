b20f4c89467e0abfa0acf0a90bfa980f22815d63
Lindsay Ward <lward23@hotmail.com>
2023-05-10 13:40:22 -0700

    Implement logic to write file to lighthouse folder

--- fetcher.js
+++ fetcher.js
@@ -6,4 +6,30 @@
 // -> Upon completion, print out "Downloaded and saved 1235 bytes to ./index.html"
 
 const request = require('request');
+const fs = require('fs');
 
+const args = process.argv.slice(2);
+
+request(args[0], (error, response, body) => {
+  console.log('error', error);
+  console.log('statusCode', response && response.statusCode);
+  if (body) {
+    writeFile(body);
+  }
+})
+
+const writeFile = (body) => {
+  let totalSize = 0;
+  for (let char of body) {
+    if (typeof char === 'string') {
+      totalSize += 1;
+    }
+  }
+ 
+  fs.writeFile(`/Users/lindsayward/lighthouse/${args[1]}`, body, err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Downloaded and saved ${totalSize} bytes to ${args[1]}.`);
+  })
+}
